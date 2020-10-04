b27b429adbdc079a668cec9e4719cbb20ec12731
Ilyat1337 <miningilya2@gmail.com>
2020-10-04 14:42:46 +0300

    add demonstration project

---
+++ Demonstration/Program.cs
@@ -0,0 +1,68 @@
+﻿using Demonstration.Serialization;
+using FakerLib;
+using GeneratorInterface;
+using System;
+using System.Collections.Generic;
+
+namespace Demonstration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FakerConfig fakerConfig = new FakerConfig();
+            fakerConfig.Add<Foo, string>(foo => foo.City, new CityGenerator());
+            Faker faker = new Faker(fakerConfig);
+
+            Foo a = faker.Create<Foo>();
+
+            ISerializer jsonSerializer = new JSONSerializer();
+            Console.WriteLine(jsonSerializer.Serizlize(a));
+        }
+    }
+
+    class CityGenerator : IGenerator
+    {
+        private static readonly List<string> cityList = new List<string>() { "Minsk", "Brest", "Grodno", "Gomel", "Vitebsk" };
+
+        private readonly Random random = new Random();
+
+        public object Generate()
+        {
+            return cityList[random.Next(cityList.Count)];
+        }
+    }
+
+    class Foo
+    {
+        public string Str
+        {get; set;}
+        public string City
+        { get; }
+        public double doubleValue;
+        private int intValue;
+        public List<Bar> bars;
+
+        public Foo() { }
+
+        public Foo(string City)
+        {
+            this.City = City;
+        }
+
+        public void SetC(int intValue)
+        {
+            this.intValue = intValue;
+        }
+
+    }
+
+    class Bar
+    {
+        public DateTime date;
+        public byte byteValue
+        { get; set; }
+
+        public Foo foo;
+    }
+}
---
+++ Demonstration/Serialization/JSONSerializer.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Demonstration.Serialization
+{
+    class JSONSerializer : ISerializer
+    {
+        public string Serizlize(object o)
+        {
+            var settings = new Newtonsoft.Json.JsonSerializerSettings() { ContractResolver = new AllFieldsContractResolver() };
+            settings.Formatting = Formatting.Indented;
+            return JsonConvert.SerializeObject(o, settings);// JsonSerializer.Serialize(o, o.GetType(), options);
+        }
+    }
+
+    public class AllFieldsContractResolver : DefaultContractResolver
+    {
+        protected override IList<Newtonsoft.Json.Serialization.JsonProperty> CreateProperties(Type type, Newtonsoft.Json.MemberSerialization memberSerialization)
+        {
+            var props = type
+                .GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Select(p => base.CreateProperty(p, memberSerialization))
+                .Union(type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Select(f => base.CreateProperty(f, memberSerialization)))
+                .Where(p => !p.PropertyName.Contains("k__BackingField"))
+                .ToList();
+            props.ForEach(p => { p.Writable = true; p.Readable = true; });
+            return props;
+        }
+    }
+
+}
